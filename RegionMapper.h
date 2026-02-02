3ff353f3c9aac4d0214b1ed8fbee687cdc422cf8
superbunny38 <superbunny38@gmail.com>
2026-02-02 16:52:23 -0500

    ㅇㅏ 진짜 제발 할 수 있는 코딩 프젝 주세요 제발 ㅠㅠ

--- RegionMapper.h
+++ RegionMapper.h
@@ -13,7 +13,25 @@ using namespace std;
 class RegionMapper {
     public:
         void LoadFromFile(const string& filename){
-            //
+            //Read file inro buffer
+            ifstream file(filename, ios::binary | ios::ate);
+            if (!file) {
+                cerr << "Error opening file: " << filename << endl;
+                return;
+            }
+            streamsize fileSize = file.tellg();
+            dataBuffer.resize(fileSize);
+            file.seekg(0, ios::beg);
+            file.read(dataBuffer.data(), fileSize);
+
+            //Parse buffer line by line
+            char* ptr = dataBuffer.data();
+            char* end = ptr + fileSize;
+
+            while(ptr<end){
+                char* lineStart = ptr;
+
+            }
         }
 
         //O(1) lookup: Returns the name corresponding to the given ID. (Returns a view into the main buffer)
